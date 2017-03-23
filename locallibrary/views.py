9a8186008d9b0dc191189e8236f603dab52953aa
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-03-23 10:41:29 -0300

    render is not used. remove a 'print'.

--- locallibrary/views.py
+++ locallibrary/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
 
-def show_pdf(request, pdf_file):
-    print(pdf_file)
+
+# request is not used
+def show_pdf(pdf_file):
     path_to_pdf = 'media/documents/' + pdf_file
     with open(path_to_pdf, 'rb') as ebook:
         response = HttpResponse(ebook.read(), content_type='application/pdf')
