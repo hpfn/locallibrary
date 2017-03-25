286e27003d9a1795e6b98d249ed947eb0cf1d826
Herbert Fortes <hpfn@users.noreply.github.com>
2017-03-25 10:02:26 -0300

    request arg must be present.

--- locallibrary/views.py
+++ locallibrary/views.py
@@ -1,9 +1,10 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# request is not used
-def show_pdf(pdf_file):
+# request is not used, but is necessary
+# 'got multiple values for argument' error
+def show_pdf(request, pdf_file):
     path_to_pdf = 'media/documents/' + pdf_file
     with open(path_to_pdf, 'rb') as ebook:
         response = HttpResponse(ebook.read(), content_type='application/pdf')
