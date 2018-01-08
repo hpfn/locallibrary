e5c9e7a6bba592cd8d3b1cf11d2089a3be4f4257
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-01-08 13:43:53 -0200

    get pdf file by attr title works. fix regex

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -19,14 +19,16 @@ from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from .views import show_pdf
+from locallibrary.views import show_pdf
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^catalog/', include('catalog.urls')),
     url(r'^$', RedirectView.as_view(url='/catalog', permanent=True)),
-    url(r'^media/documents/(?P<pdf_file>[-\w]+\.pdf)$', show_pdf, name='show_pdf'),
+    # url(r'^media/documents/(?P<pdf_file>[-\w]+\.pdf)$', show_pdf, name='show_pdf'),
+    url(r'^media/documents/(?P<pdf_file>.*)$', show_pdf, name='show_pdf'),
+    # url(r'^library/(?P<pdf_file>.*)$', show_pdf, name='show_pdf'),
     url(r'^media/(?P<path>.*\.jpeg)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
--- locallibrary/views.py
+++ locallibrary/views.py
@@ -5,13 +5,13 @@ from django.http import HttpResponse
 # request is not used, but is necessary
 # 'got multiple values for argument' error
 def show_pdf(request, pdf_file):
-    # from catalog.models import Book
+    from catalog.models import Book
     # file = Book.objects.get(title='TITLE HERE')
-    # file = Book.objects.get(title=pdf_file)
-    # print(file.ebook.path)
-    # path_to_pdf = file.ebook.path
-    from django.conf import settings
-    path_to_pdf = settings.MEDIA_ROOT + '/documents/' + pdf_file
+    file = Book.objects.get(title=pdf_file)
+    #print(file.ebook.path)
+    path_to_pdf = file.ebook.path
+    #from django.conf import settings
+    #path_to_pdf = settings.MEDIA_ROOT + '/documents/' + pdf_file
     with open(path_to_pdf, 'rb') as ebook:
         response = HttpResponse(ebook.read(), content_type='application/pdf')
         response['Content-Disposition'] = "inline; filename=" + pdf_file
