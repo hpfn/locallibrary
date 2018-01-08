9f6d06fc183549de1e80a19d530e6047fc4aec17
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-01-08 14:10:20 -0200

    replace ' ' '_' - complete name when downloading

--- locallibrary/views.py
+++ locallibrary/views.py
@@ -4,15 +4,15 @@ from django.http import HttpResponse
 
 # request is not used, but is necessary
 # 'got multiple values for argument' error
-def show_pdf(request, pdf_file):
+def show_pdf(request, book_title):
     from catalog.models import Book
     # file = Book.objects.get(title='TITLE HERE')
-    file = Book.objects.get(title=pdf_file)
+    file = Book.objects.get(title=book_title)
     #print(file.ebook.path)
     path_to_pdf = file.ebook.path
     #from django.conf import settings
     #path_to_pdf = settings.MEDIA_ROOT + '/documents/' + pdf_file
     with open(path_to_pdf, 'rb') as ebook:
         response = HttpResponse(ebook.read(), content_type='application/pdf')
-        response['Content-Disposition'] = "inline; filename=" + pdf_file
+        response['Content-Disposition'] = "inline; filename=" + book_title.replace(' ', '_')
         return response
