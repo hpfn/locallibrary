dcc5585fb571586819d18d4672616200dcb9486c
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-01-08 10:59:08 -0200

    first steps to use title to get the path to pdf file

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^catalog/', include('catalog.urls')),
     url(r'^$', RedirectView.as_view(url='/catalog', permanent=True)),
-    url(r'^media/documents/(?P<pdf_file>[-\w]+\.pdf)$', show_pdf),
+    url(r'^media/documents/(?P<pdf_file>[-\w]+\.pdf)$', show_pdf, name='show_pdf'),
     url(r'^media/(?P<path>.*\.jpeg)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
