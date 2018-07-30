5dfabfefcf5c44523b3e5265fbfad0ed6ca33973
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-07-30 13:17:30 -0300

    Update urls.py - DJ 2.0
    close #3

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,13 +2,13 @@ from .views import index
 from .views import BookListView, BookDetailView
 from .views import AuthorsListView
 from .views import SelectBooksListView
-from django.conf.urls import url
+from django.urls import path, re_path
 
 
 urlpatterns = [
-        url(r'^$', index, name='index'),
-        url(r'^books/$', BookListView.as_view(), name='books'),
-        url(r'^books/(?P<pk>\d+)$', BookDetailView.as_view(), name='book-detail'),
-        url(r'^authors/$', AuthorsListView.as_view(), name='authors'),
-        url(r'^select_books/$', SelectBooksListView.as_view(), name='select_books'),
+        path('', index, name='index'),
+        path('books/', BookListView.as_view(), name='books'),
+        re_path(r'^books/(?P<pk>\d+)$', BookDetailView.as_view(), name='book-detail'),
+        re_path(r'^authors/$', AuthorsListView.as_view(), name='authors'),
+        re_path(r'^select_books/$', SelectBooksListView.as_view(), name='select_books'),
 ]
